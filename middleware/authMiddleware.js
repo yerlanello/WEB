3dddc9c7ff5af65aefa6180320311685de1ddfa0
yerlanello <yerlansmith01@gmail.com>
2025-02-25 12:49:14 +0500

    Fix redirect when token is missing/invalid, now displays a notification

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,22 +2,23 @@ const tokenController = require('../controllers/tokenController');
 require("dotenv").config();
 
 const authenticate = async (req, res, next) => {
-
   try {
     const accessToken = req.cookies.accessToken;
-    if(!accessToken){
-      return res.status(401).json({ message: 'No access token provided' });
+    if (!accessToken) {
+      return res.redirect('/login?message=No access token provided');
     }
+
     const userData = tokenController.validateAccessToken(accessToken);
-    if(!userData){
-      return res.status(401).json({ message: 'Invalid token' });
+    if (!userData) {
+      return res.redirect('/login?message=Please login');
     }
+
     req.user = userData;
     next();
   } catch (error) {
     res.clearCookie("refreshToken");
     res.clearCookie("accessToken");
-    res.status(401).json({ message: 'Unauthorized' }); 
+    return res.redirect('/login?message=Unauthorized');
   }
 };
 
